a66b821d773fe0818fe4745f4b9f424c180d10a6
marimo2880 <marimo2880@gmail.com>
2013-04-10 22:16:54 +0900

    Add get_priority_list function.

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include "support.h"
 
+struct cp_info
+{
+    int cost;
+    int number;
+};
+
+typedef struct cp_info cp_info;
 
 int get_exit_task(void)
 {
@@ -11,11 +18,11 @@ int get_exit_task(void)
     return exit_task_no;
 }
 
-int* get_critical_path(void)
+int *get_critical_path(void)
 {
     int i, j;
     //allocate cp
-    int* cp = malloc(total_task * sizeof(int));
+    int *cp = malloc(total_task * sizeof(int));
     //initialize cp
     memset(cp, 0, total_task * sizeof(int));
     cp[get_exit_task()] = task[get_exit_task()].cost;
@@ -32,9 +39,37 @@ int* get_critical_path(void)
     return cp;
 }
 
+int compare_cp_info(const void* p_a, const void* p_b)
+{
+    const cp_info* a = (cp_info*)p_a;
+    const cp_info* b = (cp_info*)p_b;
+    //cp_info a = *casted_p_a;
+    //cp_info b = *casted_p_b;
+
+    if(a->cost == b->cost) 
+        return 0;
+    else if(a->cost > b->cost)
+        return -1;
+    else 
+        return 1;
+}
+
 void get_priority_list(void)
 {
-    
+    int i;
+    int *cp = get_critical_path();
+    cp_info *priority_list = malloc(total_task * sizeof(cp_info));
+    for (i = 0; i < total_task; i++)
+    {
+        priority_list[i].cost = cp[i];
+        priority_list[i].number = i;
+    }
+    qsort(priority_list,total_task,sizeof(cp_info),&compare_cp_info);
+
+    for(i = 0; i < total_task; i++)
+    {
+        printf("%d:%d\n",priority_list[i].number,priority_list[i].cost );
+    }
 }
 
 
@@ -45,8 +80,7 @@ int main(int argc, char *argv[])
 
     /*scheduling start*/
 
-    get_exit_task();
-    get_critical_path();
+    get_priority_list();
 
     /*scheduling end*/
 
